149988f22237f35b025b12c4e6fe7e736bdf8224
Satyam <tsingh30399@gmail.com>
2023-05-17 19:32:08 +0530

    Time extended

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const endDate = "18 May 2023 2:00 PM"
+const endDate = "18 May 2024 2:00 PM"
 
 document.getElementById("end-date").innerText = endDate
 const inputs = document.querySelectorAll('input')
